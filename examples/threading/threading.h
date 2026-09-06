6b467627f2b97c49aa83e52914cc89910893daf8
malachiw <malachi.woodlee@gmail.com>
2026-09-05 21:59:26 -0400

    threading, my man, with a lot of struggle because my posix c is terrible.

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -14,6 +14,11 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
+    int thread_id;
+    int wait_ms;
+    int release_ms;
+    pthread_mutex_t *mutex;
+
 
     /**
      * Set to true if the thread completed with success, false
